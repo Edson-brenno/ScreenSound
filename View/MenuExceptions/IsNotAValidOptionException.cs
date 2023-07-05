ce88c6b05859f88ecb622d7c539729898f05ab91
brenno <brennorebeiro@gmail.com>
2023-07-04 23:26:43 -0300

    Add Exception Para opção do menu inválida

---
+++ View/MenuExceptions/IsNotAValidOptionException.cs
@@ -0,0 +1,10 @@
+// Invalid menu option exception; Opção do menu inválida
+namespace ScreenSound.View.MenuException{
+    public class IsNotValidOptionException : Exception{
+        public IsNotValidOptionException(){
+            System.Console.WriteLine("Opção Inválida");
+        }
+
+        public IsNotValidOptionException(string message) : base(message){}
+    }
+}
