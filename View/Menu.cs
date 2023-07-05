14062cc09f156f9d988d41f73e540f4ff122366a
brenno <brennorebeiro@gmail.com>
2023-07-04 23:59:11 -0300

    Add escolha menu

--- View/Menu.cs
+++ View/Menu.cs
@@ -1,8 +1,11 @@
 // ScreenSounds Menu
 
+//Menu exceptions:
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using ScreenSound.View.MenuException;
 
-namespace ScreenSound.View{
+namespace ScreenSound.View.Menu{
 
     public class Menu{
 
@@ -21,6 +24,61 @@ namespace ScreenSound.View{
             System.Console.WriteLine("2. Mostrar bandas registradas");
             System.Console.WriteLine("3. Avaliar uma banda");
             System.Console.WriteLine("4. Ver média de uma banda");
+            System.Console.WriteLine("5. Sair da ScreenSound");
+        }
+
+        private void EscolhaOpcaoMenu(){ // Menu choosed option; Escolha opção menu
+            bool continuaEscolhaOpcao = true;
+            string opcao = "";
+
+            while(continuaEscolhaOpcao == true){
+                
+                try {
+
+                    System.Console.Write("\n Digite a opção escolhida: ");
+
+                    opcao = System.Console.ReadLine();
+
+                    // Validations about the choosed option; Validação da opção escolhida
+                    if (String.IsNullOrEmpty(opcao) || String.IsNullOrWhiteSpace(opcao)){
+                        throw new NullOptionException();
+                    }
+                    else if (Char.IsBetween(char.ToLower(opcao[0]),'a','z') == true ){
+                        throw new IsNotValidOptionException(opcao);
+                    }
+                    else if (Char.IsDigit(opcao[0]) == true && int.Parse(opcao) > 5) {
+                        throw new IsNotValidOptionException();
+                    }
+                    else{
+                        continuaEscolhaOpcao = false;
+                    }
+                }catch(Exception ex){
+                    System.Console.WriteLine(ex.Message);
+                }
+                
+            }
+
+            switch(int.Parse(opcao)){
+                case 1:
+                    System.Console.WriteLine("Registrando uma banda");
+                    break;
+                case 2:
+                    System.Console.WriteLine("Mostrando bandas registradas");
+                    break;
+                case 3:
+                    System.Console.WriteLine("Avaliando bandas");
+                    break;
+                case 4:
+                    System.Console.WriteLine("Media da banda");
+                    break;
+                case 5:
+                    System.Console.WriteLine("Tchau :)");
+                    break;
+                default:
+                    System.Console.WriteLine("Opção inválida");
+                    break;
+            }
+
         }
 
         public void Main(){
@@ -28,6 +86,8 @@ namespace ScreenSound.View{
             this.ApresentaMenu();
 
             this.MostraOpcoesMenu();
+
+            this.EscolhaOpcaoMenu();
             
         }
     }
