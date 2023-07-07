e85b39a150ae956df32c17b5a0c852413ff9315e
brenno <brennorebeiro@gmail.com>
2023-07-07 13:58:36 -0300

    Add RegistrarBanda

---
+++ Controller/RegistrarBanda.cs
@@ -0,0 +1,8 @@
+namespace ScreenSound.Controller{
+    public class RegistrarBanda{
+
+        public static void Registrar(){
+            System.Console.WriteLine("Registrando banda");
+        }
+    }
+}
