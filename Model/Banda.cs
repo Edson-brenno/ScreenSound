33e765186df56f4bafcbb0869bf473aa75562371
brenno <brennorebeiro@gmail.com>
2023-07-07 13:58:09 -0300

    Add model banda

---
+++ Model/Banda.cs
@@ -0,0 +1,11 @@
+namespace ScreenSound{
+    class Banda {
+        //Nome banda
+        public string nome;
+
+        //To String:
+        public override string ToString(){
+            return $"{this.nome}";
+        }
+    }
+}
--- Model/RegistrarBanda.cs
+++
@@ -1,8 +0,0 @@
-namespace ScreenSound.Model{
-    public class RegistrarBanda{
-
-        public static void Registrar(){
-            System.Console.WriteLine("Registrando banda");
-        }
-    }
-}
