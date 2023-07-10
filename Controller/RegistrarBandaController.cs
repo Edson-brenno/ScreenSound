476eaa1ab2b69f14cc63a5fe15fa761fbe73fcb3
brenno <brennorebeiro@gmail.com>
2023-07-10 18:52:34 -0300

    Add DoesNomeBandaExists

--- Controller/RegistrarBandaController.cs
+++ Controller/RegistrarBandaController.cs
@@ -2,10 +2,10 @@ using ScreenSound.Model;
 using Newtonsoft;
 using Newtonsoft.Json;
 using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices;
 
 namespace ScreenSound.Controller{
     public class RegistrarBandaController{
-
         public static void Registrar(){
             Banda banda = new Banda();
             banda.nome = "Brenno";
@@ -20,6 +20,24 @@ namespace ScreenSound.Controller{
             System.Console.WriteLine(tt);
 
             t2.ForEach(x => Console.WriteLine(x));
+        }
+
+        public static bool DoesNomeBandaExists(string nome){
+            
+            if(!File.Exists("tt.json")){
+                return false;
+            }
+            else{
+                using (StreamReader r = new StreamReader("tt.json")){
+                    string jsonFile = r.ReadToEnd();
+
+                    List<Banda> bandas = JsonConvert.DeserializeObject<List<Banda>>(jsonFile);
+
+                    System.Console.WriteLine(bandas);
+                }
+            }
+            
+            return false;
         }
     }
 }
