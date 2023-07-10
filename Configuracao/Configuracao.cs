80982086754297e517f7922f4ce2df67027f4c3e
brenno <brennorebeiro@gmail.com>
2023-07-10 19:19:33 -0300

    Add Configurar

--- Configuracao/Configuracao.cs
+++ Configuracao/Configuracao.cs
@@ -3,7 +3,7 @@ namespace ScreenSound.Configuracao{
     public class ConfiguracaoScreenSound{ //This class will be used for screenSound First configuration; Classe a ser usada caso o screensound não foi configurado
 
         
-        public static bool ScreenSoundJaFoiConfigurado(){ // Check if the application was already configured; Verifica se a aplicação já foi configurada
+        public static bool JaFoiConfigurado(){ // Check if the application was already configured; Verifica se a aplicação já foi configurada
             if (File.Exists("tt.json")){
                 return true;
             }
@@ -14,6 +14,7 @@ namespace ScreenSound.Configuracao{
 
         public static void Configurar(){ //Do the configuration of the application; Faz a configuração da aplicação
             
+            using (File.CreateText("tt.json")){};
 
         }
     }
