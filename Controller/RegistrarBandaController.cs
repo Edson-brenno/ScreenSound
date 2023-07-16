e096fc93caacdee17d0dd70ac6e3623d4ec6392d
brenno <brennorebeiro@gmail.com>
2023-07-16 20:40:35 -0300

    Fix problem is not possible deserialize list

--- Controller/RegistrarBandaController.cs
+++ Controller/RegistrarBandaController.cs
@@ -24,25 +24,47 @@ namespace ScreenSound.Controller{
             try{
                 // Read the bands json; Leitura do json das bandas
                 using (StreamReader r = new StreamReader("bandas.json")){
+                    //Read the json to End
                     string jsonFile = r.ReadToEnd();
 
-                    //Deserialize json; Descerialização do json
-                    List<Banda> bandas = JsonConvert.DeserializeObject<List<Banda>>(jsonFile);
+                    //If the json has already more than 1 band registred; Se o json tem mais de uma banda registrada
+                    if (jsonFile.StartsWith("[")){
 
-                    // Verify if the json is already populated; verifica se o json possui algum dado
-                    if (bandas != null && bandas.Count > 0){
-                        // Check the bands name already exist; verifi se o nome da banda existe
-                        if (bandas.Contains(new Banda{nome = nomeBanda})){
-                            return true;
+                        //Deserialize json; Descerialização do json
+                        List<Banda> bandas = JsonConvert.DeserializeObject<List<Banda>>(jsonFile);
+
+                        // Verify if the json is already populated; verifica se o json possui algum dado
+                        if (bandas != null && bandas.Count > 0){
+                            // Check the bands name already exist; verifi se o nome da banda existe
+                            if (bandas.Contains(new Banda{nome = nomeBanda})){
+                                return true;
+                            }
+                            else{
+                                return false;
+                            }
+                        }
+                        // If the jason does not have value; Se o json não possui dados
+                        else{
+                            return false;
+                        }
+                    }
+                    // If the json only have one band; Se o json tem apenas uma banda
+                    else{
+                        Banda banda = JsonConvert.DeserializeObject<Banda>(jsonFile);
+
+                        if (banda.nome != null){
+                            if (banda.nome.Contains(nomeBanda[0])){
+                                return true;
+                            }
+                            else{
+                                return false;
+                            }
                         }
                         else{
                             return false;
                         }
                     }
-                    // If the jason does not have value; Se o json não possui dados
-                    else{
-                        return false;
-                    }
+                    
                 }
             }
             catch(Exception ex){
