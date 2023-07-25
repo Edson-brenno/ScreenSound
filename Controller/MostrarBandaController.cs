10be6c5e42dd8810f230d45f87151e10b75f4d01
brenno <brennorebeir@gmail.com>
2023-07-24 21:58:59 -0300

    Adiciona controller mostrar bandas

--- Controller/MostrarBandaController.cs
+++ Controller/MostrarBandaController.cs
@@ -1,5 +1,30 @@
+using System;
+using System.IO;
+using System.Linq;
+using ScreenSound.Model;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+
 namespace ScreenSound.Controller{
     public class MostrarBandaController{
         
+        private List<Banda> bandas;
+        public MostrarBandaController(){
+
+            string jsonFile = File.ReadAllText("bandas.json");
+
+            this.bandas = JsonConvert.DeserializeObject<List<Banda>>(jsonFile);
+        }
+        public void Mostrar(){
+
+            int numero = 1;
+            this.bandas.ForEach(banda => {
+                System.Console.WriteLine($"{numero} ---- {banda.nome}"); 
+                numero++;
+                    
+            });
+            
+        } 
     }
 }
