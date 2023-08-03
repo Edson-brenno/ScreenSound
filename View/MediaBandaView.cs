23d2664fb8c15f650d290913e78d9938dc0f5867
brenno <brennorebeir@gmail.com>
2023-08-02 21:42:48 -0300

    Adiciona apresentacao media banda

---
+++ View/MediaBandaView.cs
@@ -0,0 +1,21 @@
+using ScreenSound.Controller;
+using ScreenSound.View.MenuException;
+using ScreenSound.ExceptionAvaliarBanda;
+using ScreenSound.View.Exceptions;
+
+namespace ScreenSound.View{
+    public class MediaBandaView{
+        public static void ApresentaMenuMediaBanda(){
+            System.Console.Clear();
+
+            System.Console.WriteLine(@"
+
+███████████████████████████████████████████████████████████████████████
+█▄─▀█▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄██▀▄─████▄─▄─▀██▀▄─██▄─▀█▄─▄█▄─▄▄▀██▀▄─██─▄▄▄▄█
+██─█▄█─███─▄█▀██─██─██─███─▀─█████─▄─▀██─▀─███─█▄▀─███─██─██─▀─██▄▄▄▄─█
+▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▀▀▄▄▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▄▀" + "\n\n");
+        }
+
+
+    }
+}
